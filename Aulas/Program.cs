e0be08dc2e32da963335769236cf1b6234959bca
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-04-15 16:15:27 -0300

    Add files via upload

---
+++ Aulas/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula01;
+
+public class Program
+{
+    public static void Main()
+    {
+        double numberD = 10.50;
+        int number = 19;
+        string myName = "Izabelly";
+        char character = 'a';
+        bool boolean = true;
+        const int numb = 10;
+        var variable = "iza";
+        dynamic numero = 20;
+
+        Console.WriteLine(myName);
+        Console.WriteLine(number);
+        Console.WriteLine("O numero é: " + numberD);
+
+    }
+}
