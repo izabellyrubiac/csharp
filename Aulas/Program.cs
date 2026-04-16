c6f1b3f0efb51424d1b6d84efb0075f6be3f80cd
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-04-16 18:05:55 -0300

    Add files via upload

---
+++ Aulas/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula02;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("---------CADASTRO DE USUARIO------------");
+
+        //Solicitar o nome do usuario
+        Console.WriteLine("Digite seu nome: ");
+        string name = Console.ReadLine();
+
+        //Solicitar a idade do usuario
+        Console.WriteLine("Digite sua Idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("-----------------------------------");
+
+        Console.WriteLine("Nome do Usuário: " + name);
+        Console.WriteLine("Idade do Usuário: " + age);
+    }
+}
