957028b2cb0e4dab2d502747ae325de4368953e2
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-04-17 20:26:35 -0300

    Add files via upload

---
+++ Aulas/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula04
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("---------VERIFICADOR DE NUMEROS PARES OU IMPARES--------");
+            Console.WriteLine("DIGITE UM NUMERO: ");
+            int number = Convert.ToInt32 (Console.ReadLine());
+
+            int result = number % 2;
+
+            if(result == 0)
+            {
+                Console.WriteLine("O numero é par!");
+
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar!");
+            }
+        }
+    }
+}
