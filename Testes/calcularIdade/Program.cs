15b3be50a6c842b9c882cd44ce1cb6454dc3836c
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-04-25 03:20:24 -0300

    Add files via upload

---
+++ Testes/calcularIdade/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite seu ano de nascimento: ");
+int ano = Convert.ToInt32(Console.ReadLine()!);
+int anoAtual = DateTime.Now.Year;
+int idade = anoAtual - ano;
+Console.WriteLine($"Sua idade é {idade}!");
