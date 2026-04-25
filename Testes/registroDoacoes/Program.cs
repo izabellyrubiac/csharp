4f82bbd5a8f50873a3d9ed683e8bca356bee0e24
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-04-25 03:22:49 -0300

    Add files via upload

---
+++ Testes/registroDoacoes/Program.cs
@@ -0,0 +1,32 @@
+﻿
+    Console.Write("Digite o valor recebido: ");
+    decimal valorDoado = decimal.Parse(Console.ReadLine()!);
+
+
+    Console.Write("\nDoação anônima? Digite 'SIM' ou 'NAO': ");
+    string sn = (Console.ReadLine()!.ToLower());
+    bool anonimo = (sn == "sim");
+
+
+    Console.Write("Qual tipo de conta? Digite: (C) para Corrente ou (P) para poupança: ");
+    char cp = char.Parse(Console.ReadLine()!.ToLower());
+
+Console.WriteLine("Valor recebido: R$" + valorDoado);
+Console.WriteLine("Doação anônima: " + anonimo); //Retorna True or False
+
+
+if (cp == 'c')
+{
+    Console.WriteLine("Tipo de conta: C");
+}
+else if (cp == 'p')
+{
+    Console.WriteLine("Tipo de conta: P");
+}
+else
+{
+    Console.WriteLine("Opcao inválida do tipo de conta! Por favor escolha a opção correta para continuar com o registro.");
+}
+
+
+
