07803df8e4a7f053e6b0b8a7b806dc922c12861b
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-04-28 08:16:41 -0300

    Add files via upload

---
+++ Testes/Program.cs
@@ -0,0 +1,5 @@
+﻿decimal conversao = 1.60934m;
+Console.Write("Digite quantas milhas: ");
+decimal milhas = long.Parse(Console.ReadLine()!);
+decimal milhaToKm = (milhas * conversao);
+Console.WriteLine($"\n{milhas} milhas equivalem a {milhaToKm} quilômetros");
