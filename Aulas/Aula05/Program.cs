43e898858b0140f5926ae8cafe7df5ec23b82f94
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-04-28 08:18:48 -0300

    Add files via upload

---
+++ Aulas/Aula05/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Aula05
+{
+    public class Program()
+    {
+        public static void Main()
+        {
+            Console.WriteLine("=====Operadores Relacionais=====\n");
+            Console.WriteLine("Digite o primeiro número: ");
+            int x = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int y = Convert.ToInt32 (Console.ReadLine());
+
+            bool condicao = x == y;
+
+            string result;
+
+            //Operador de Igualdade (==)
+            if (x == y)
+            {
+                result = "x é igual a y";
+            }
+            else
+            {
+                result = "x é diferente de y";
+            }
+
+            Console.WriteLine("\nIgualdade (==): " + result);
+
+        }
+    }
+}
