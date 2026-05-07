4deb2867aa0ffd74a53598bbab693f693235887a
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-05-07 17:32:20 -0300

    Add files via upload

---
+++ Testes/calculoArea/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.Write("Digite a largura: ");
+double largura = double.Parse(Console.ReadLine()!);
+Console.Write("Digite o comprimento: ");
+double comprimento = double.Parse(Console.ReadLine()!);
+double area = largura * comprimento;
+Console.WriteLine($"A area do terro é : {area} metros quadrados.");
