17bce7486f9a4ac9d5d6c2259535ed9c7e1a490e
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2026-05-07 17:30:15 -0300

    Add files via upload

---
+++ Testes/horasMinutos/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Digite os minutos para converter em horas: ");
+
+int totalminutos = int.Parse(Console.ReadLine()!);
+int horas = totalminutos / 60;
+int minutos = totalminutos % 60;
+Console.WriteLine($"Tempo: {horas} horas e {minutos} minutos");
